837ed91cf9b67809aa33156ea649969400e47cf1
karinaqueirozfdte <karina.queiroz@fdte.org.br>
2019-04-24 13:22:16 -0300

    Estruturacao-de-site

---
+++ js/search.js
@@ -0,0 +1,7 @@
+const btnProcurar = document.querySelector('.formSearch-procurar');
+btnProcurar.addEventListener('click', function(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '');
+    xhr.responseType = '';
+    xhr.send();
+})
